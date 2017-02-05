fc3d8505bf1e2082f85c01d54725b39beb6d55ab
Tyler Posadovsky <typosado@genband.com>
2017-02-05 04:01:17 -0500

    start

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/**
+ * jTinder initialization
+ */
+$("#tinderslide").jTinder({
+	// dislike callback
+    onDislike: function (item) {
+	    // set the status text
+        $('#status').html('Dislike image ' + (item.index()+1));
+        console.log("DISLIKE");
+    },
+	// like callback
+    onLike: function (item) {
+	    // set the status text
+        $('#status').html('Like image ' + (item.index()+1));
+        console.log("LIKE");
+    },
+	animationRevertSpeed: 200,
+	animationSpeed: 400,
+	threshold: 1,
+	likeSelector: '.like',
+	dislikeSelector: '.dislike'
+});
+
+/**
+ * Set button action to trigger jTinder like & dislike.
+ */
+$('.actions .like, .actions .dislike').click(function(e){
+	e.preventDefault();
+	$("#tinderslide").jTinder($(this).attr('class'));
+});
